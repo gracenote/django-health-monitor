4f277041780d404d9a65bd038050eecc6bb3c8fe
Sean Chon <schon@gracenote.com>
2017-01-24 11:30:51 -0800

    Update pip requirements and add django-jsonfield to install_requires.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     url='https://github.com/seanchon/django-health-monitor',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["django-model-utils>=2.0", "jsonfield"],
+    install_requires=["django-model-utils>=2.0", "django-jsonfield", "jsonfield"],
     license="MIT",
     zip_safe=False,
     keywords='django-health-monitor',
