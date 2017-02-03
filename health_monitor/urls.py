4b06210ddcff50e9aa99a86edeb99e1a6200d5a9
Sean Chon <schon@gracenote.com>
2017-02-02 18:54:11 -0800

    Modify UID to be passed as number.

--- health_monitor/urls.py
+++ health_monitor/urls.py
@@ -3,7 +3,7 @@ from health_monitor import views
 
 
 urlpatterns = [
-    url(r'^health/(?P<uid>[\w-]*)/$', views.read, name='read'),
-    url(r'^health/(?P<uid>[\w-]*)/update/(?P<test_name>[\w-]*)/$', views.update, name='update'),
-    url(r'^health/(?P<uid>[\w-]*)/history/(?P<group>[\w-]*)/$', views.history, name='history'),
+    url(r'^health/(?P<uid>[\d]*)/$', views.read, name='read'),
+    url(r'^health/(?P<uid>[\d]*)/update/(?P<test_name>[\w-]*)/$', views.update, name='update'),
+    url(r'^health/(?P<uid>[\d]*)/history/(?P<group>[\w-]*)/$', views.history, name='history'),
 ]
